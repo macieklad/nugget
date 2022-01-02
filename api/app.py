642ccefa4f8ec64fb8871d545f3ca08f27128af2
Maciej Ładoś <maciej@lados.dev>
2022-01-02 20:34:08 +0100

    chore: additional error handlers

--- api/app.py
+++ api/app.py
@@ -1,19 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
-
 from .src.http.routes import routes
-
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 CORS(app)
 
 
-@app.errorhandler(404)
+@ app.errorhandler(500)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 500
+
+
+@ app.errorhandler(404)
 def resource_not_found(e):
     return jsonify(error=str(e)), 404
 
 
-@app.errorhandler(400)
+@ app.errorhandler(400)
 def bad_request(e):
     return jsonify(error=str(e)), 404
